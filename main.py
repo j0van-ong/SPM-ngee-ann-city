6261c9d1f39dbd8995136f0ca35c0d02ca63c4a5
yongkaiyu <150000961+yongkaiyu@users.noreply.github.com>
2024-05-30 19:57:53 +0800

    updated main menu

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-print("--------------------------------")
-print("- (1) Start New Arcade Game    -")
-print("- (2) Start New Free Play Game -")
-print("- (3) Load New Saved Game      -")
-print("- (4) Display High Scores      -")
-print("- (0) Exit Game                -")
-print("--------------------------------")
+print("----------------------------------\n"
+      "-       | Ngee Ann City |        -\n"
+      "----------------------------------\n"
+      "-  (1) Start New Arcade Game     -\n"
+      "-  (2) Start New Free Play Game  -\n"
+      "-  (3) Load New Saved Game       -\n"
+      "-  (4) Display High Scores       -\n"
+      "-  (0) Exit Game                 -\n"
+      "----------------------------------")
