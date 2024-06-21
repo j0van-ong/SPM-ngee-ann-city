45198ae5d37dab637ebdb7dc2cbc0d41c5db93f4
Kiefer <S10259865@connect.np.edu.sg>
2024-06-21 23:27:17 +0800

    Chore: Commercial Changes

--- Commercial.py
+++ Commercial.py
@@ -1,5 +1,8 @@
 import Building
 
 class Commercial(Building):
-    def __init__(self) -> None:
-        pass
+     def __init__(self):
+        super().__init__()
+        self.name = "Commercial"
+        self.upkeep_cost = 2
+        self.profit = 3
